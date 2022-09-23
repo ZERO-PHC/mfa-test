8ddb20f73dbb1d29a0f1df5f2cb78d52b1a76662
Alan Dega <zero@cyphers.gg>
2022-09-23 11:57:40 -0500

    redesgined sampler card

--- src/components/magicAcademy/SamplerCard.jsx
+++ src/components/magicAcademy/SamplerCard.jsx
@@ -11,52 +11,58 @@ export default function SamplerCard({ name, id, xp, active, handleCardSelection,
     const { getOrbiesData } = useMagicSchoolSteps()
 
     const handleCardClick = async () => {
-        await getOrbiesData() 
+        await getOrbiesData()
         router.push(path)
     }
 
     return (
         <Wrapper onClick={handleCardSelection}>
-               <section>
-                <div className='sampler-name'>
-                    {name}
+            <div className='header'>
+                <div style={{ width: '40%', height: '100%', position: "relative", borderRight: "1px solid lightgrey" }}>
+                    <div className='sampler-name'>
+                        {name}
+                    </div>
+                    <Image src="/assets/preview.png" alt="preview" layout="fill" objectPosition={"center"} />
                 </div>
-                <Image src="/assets/preview.png" alt="preview" layout="fill" objectFit='cover' objectPosition={"center"} />
-            </section>
+                <div style={{ width: '60%', height: '100%', padding:"0.5rem 1rem" }}>
+                    description
+                </div>
+            </div >
             <div className='content-wrapper'>
                 <div className='header-title'>
                     <span>
-
                         SAMPLER LVL {id + 1}
                     </span>
-                    <div>
-                        <svg width="100%" height="8" viewBox="0 0 422 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect y="7" width="214" height="1" fill="#D9D9D9" />
-                            <rect x="225" width="197" height="1" fill="#D9D9D9" />
-                            <path d="M214 7L225 0V1L214 8V7Z" fill="#D9D9D9" />
-                        </svg>
-
-                    </div>
-
                 </div>
                 <div className='content'>
-                    <div >
                         <ul>
                             <li>
-                                {xp} XP
+                                 XP: {xp}
                             </li>
                             <li>
-                                Float lvl 1
+                                Floats:
                             </li>
                         </ul>
-                    </div>
-                    <button disabled={path == undefined ? true : false} onClick={() => handleCardClick(path)}>
-                        {path == undefined ? "Coming soon..." : "Start"}
-                    </button>
+
                 </div>
-            </div> 
-          
-            
+            </div>
+
+            <footer>
+                <div style={{ width: '100%' }}>
+                    <svg width="100%" height="8" viewBox="0 0 422 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect y="7" x='-214' width="428" height="1" fill="#D9D9D9" />
+                        <rect x="225" width="497" height="1" fill="#D9D9D9" />
+                        <path d="M214 7L225 0V1L214 8V7Z" fill="#D9D9D9" />
+                    </svg>
+
+                </div>
+                <button disabled={path == undefined ? true : false} onClick={() => handleCardClick(path)}>
+                    {path == undefined ? "Coming soon..." : "Start"}
+                </button>
+            </footer>
+
+
+
         </Wrapper>
     )
 }
@@ -70,9 +76,9 @@ const Wrapper = styled.main`
   margin:2rem;
   display:flex;
   flex-direction:column;
-  align-items:center;
+//   align-items:center;
   font-family:"Michroma", sans-serif;
-    border: 1px solid #D9D9D9;
+    // border: 1px solid #D9D9D9;
   animation: fadeIn 0.5s ease-in;
   delay:2s;
     @keyframes fadeIn {
@@ -93,32 +99,46 @@ const Wrapper = styled.main`
     position:absolute;
     text-transform:uppercase;
     top:0.5rem;
-    left:0rem;
+    left:0.5rem;
     z-index:1;
+  }
+
+  ul { 
+    display:flex;
+    width:100%;
+    justify-content:space-between;
   }
 
   span{
     margin:0.3rem;
   }
 
-  section{
+  .header{
     position:relative;
     display:flex;
-    justify-content:center;
+    justify-content:space-between;
     align-items:center;
     height:50%;
     width:100%;
   }
 
+  footer {
+    height: 3%;
+    text-align: right;
+
+    // button  {
+    //     margin: 0.5rem 1rem;}
+  }
+
   .content-wrapper {
     display:flex;
-    flex-direction:column;
-    height:50%;
+    flex-direction:row;
+    height:30%;
     width:100%;
     border-top: 1px solid lightgrey;
   } 
   .header-title{
-    height:50%;
+    height:30%;
     display:flex;
     flex-direction:column;
     align-items:space-between;
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   const onLoad = (spline) => {
     console.log("loaded", spline);
     // setTimeout(() => {
-    //   setLoading(false);
+      // setLoading(false);
     // }, 1500);
   };
 
